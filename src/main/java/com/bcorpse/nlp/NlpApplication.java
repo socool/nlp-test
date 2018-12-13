e4d75e191e653701aa5e77282b542a9152ec73fd
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2018-12-13 17:31:34 +0700

    first commit

---
+++ src/main/java/com/bcorpse/nlp/NlpApplication.java
@@ -0,0 +1,14 @@
+package com.bcorpse.nlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlpApplication.class, args);
+	}
+
+}
+
